c8e6742c7a75dbe64a11614bcd06bf1ed835f1d8
Nicholaus Rowe <ux@nicholausrowe.com>
2021-08-19 23:30:06 +0000

    Completed sql-join exercise.

---
+++ sql-join/big-spenders.sql
@@ -0,0 +1,10 @@
+select
+  "payments"."amount",
+  "customers"."firstName",
+  "customers"."lastName"
+from
+  "payments"
+join
+  "customers" using ("customerId")
+order by "amount" desc
+limit 10;
---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,11 @@
+select
+  "films"."releaseYear",
+  "categories"."name" as "category"
+from
+  "films"
+join
+  "filmCategory" using ("filmId")
+join
+  "categories" using ("categoryId")
+where
+  "title" = 'Boogie Amelie';
